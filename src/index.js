1173854359dcefaff7b3fad140493c4841488529
shashankgpt98 <shashankgpt07@gmail.com>
2022-03-12 03:06:38 +0530

    change in file name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import BottomSlider from "./BottomSlider/BottomSlider"
+import BottomSlider from "./Modal/Modal"
 ReactDOM.render(
   <React.StrictMode>
     <BottomSlider />
